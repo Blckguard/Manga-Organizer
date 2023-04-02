06b7c7a5a4e09334790e2dee0fe0ea4f03f29c67
Max <blckguard2000@gmail.com>
2023-04-02 09:01:14 +0900

    initial commit

---
+++ Manga-Organizer/BookHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
+
+namespace HelloWPF
+{
+    internal class BookHandler
+    {
+        public static void GetBookTitle()
+        {
+            string bookPath = @"C:\Users\candy\repos\Manga-Organizer\Manga-Organizer\Books\Abara_ Complete Deluxe Edition - Tsutomu Nihei.epub";
+
+            string extractPath = ".\\Temp";
+            string metadataPath = ".\\Temp\\OEBPS\\content.opf";
+            Directory.CreateDirectory(".\\Temp");
+            ZipFile.ExtractToDirectory(bookPath, extractPath);
+
+            string[] lines = File.ReadAllLines(metadataPath);
+            Trace.WriteLine("successfully extracted");
+            foreach (string line in lines)
+            {
+                if (line.Contains("<dc:title>"))
+                {
+                    string title = line.Substring(10, line.Length - 11);
+                    Trace.WriteLine(title);
+                    break;
+                }
+            }
+            Directory.Delete(extractPath, true);
+        }
+
+        public static void ExtractBook(string bookPath)
+        {
+            string extractPath = "C:\\Users\\candy\\repos\\C#\\HelloWPF\\Books\\Abara";
+            ZipFile.ExtractToDirectory(bookPath, extractPath);
+        }
+    }
+}
---
+++ Manga-Organizer/View/UserControls/Bookview.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWPF.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Bookview.xaml
+    /// </summary>
+    public partial class Bookview : UserControl
+    {
+        public string BookPath { get; set; }
+        public Bookview()
+        {
+            InitializeComponent();
+            BookPath = string.Empty;
+        }
+
+        private void buttonExtract_Click(object sender, RoutedEventArgs e)
+        {
+            BookHandler.GetBookTitle();
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Get the path of the dropped file
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                BookPath = files[0]; // Assuming only one file was dropped
+
+            }
+        }
+    }
+}
