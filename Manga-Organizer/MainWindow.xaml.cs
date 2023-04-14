de422e2965887dafe6aefaaa276cd8642f208038
Max <blckguard2000@gmail.com>
2023-04-14 19:11:23 +0900

    .

--- Manga-Organizer/MainWindow.xaml.cs
+++ Manga-Organizer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,8 @@ namespace Manga_Organizer
         public MainWindow()
         {
             InitializeComponent();
+            Directory.CreateDirectory(@"Temp");
+            Directory.CreateDirectory(@"Books");
         }
     }
 }
